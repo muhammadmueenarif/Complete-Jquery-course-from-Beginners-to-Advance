b77ec0c3c58b8ab72fdd06760e30a9855a6bbcb1
Mueen Arif <muhammadmueenarif786@gmail.com>
2025-01-18 14:00:02 +0500

    remove elements

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
-    $('#dynamicList').append('<li>New item</li>');
+    $('#dynamicList li:last-child').remove();
+    $('#dynamicList li:contains("item 2")').remove();
 
-    var newItemArray = ['item 3', 'item 4', 'item 5'];
-    $.each(newItemArray, function (index, value) {
-        $('#dynamicList').append('<li>' + value + '</li>')
-    });
-    //prepend means add text before that element
-    $('#contentContainer').prepend('<h3>Added heading</h3>'); 
 });
